7343c252ced53394df96ce4529b574f7c03b00a8
Moksh Upadhyay <mokshupadhyay5@gmail.com>
2022-01-28 13:40:38 +0530

    File edited

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="appComponent">
-      Moksh Upadhyay lolollolololololololololololololololol
+      By Moksh Upadhyay
     </div>
   );
 }
