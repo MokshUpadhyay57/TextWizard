02305d2f57dcc446a14844ab094bb3e238b5aa2e
Moksh Upadhyay <mokshupadhyay5@gmail.com>
2022-01-30 15:21:55 +0530

    added summary section

--- src/components/TextUtil.js
+++ src/components/TextUtil.js
@@ -6,6 +6,10 @@ const TextUtil = () => {
 
     const [text, setText] = useState('');
 
+    const onChangeHandler = (event) => {
+        setText(event.target.value)
+    }
+
     const upperCaseHandler = () => {
         console.log("UpperCase Button was clicked")
         const newText = text.toUpperCase();
@@ -13,9 +17,6 @@ const TextUtil = () => {
     }
 
 
-    const onChangeHandler = (event) => {
-        setText(event.target.value)
-    }
 
     const lowerCaseHandler = () => {
         console.log("LowerCase Button was clicked")
@@ -26,16 +27,10 @@ const TextUtil = () => {
 
     const trimSpaceHandler = () => {
         console.log("TrimSpace Button was clicked")
-
-    }
-
-
-    const countHandler = () => {
-        console.log("Count Button was clicked")
-        let newText = text.toString().split(" ").length - 1;
+        let newText = text.trim(text);
         setText(newText)
-    }
 
+    }
 
     const clearTextHandler = () => {
         console.log("Clear Text Button was clicked")
@@ -50,15 +45,25 @@ const TextUtil = () => {
                 TextUtils - For every word you type
             </div>
             <div className="mainContainer">
-                <textarea rows="20" cols="150" value={text} onChange={onChangeHandler}>Please enter some text</textarea>
+                <textarea
+                    rows="20"
+                    cols="150"
+                    value={text}
+                    onChange={onChangeHandler}>
+                    Please enter some text
+                </textarea>
             </div>
+
             <div className="functionContainer">
                 <button onClick={upperCaseHandler}>UpperCase</button>
                 <button onClick={lowerCaseHandler}>LowerCase</button>
                 <button onClick={trimSpaceHandler}>Trim Space</button>
-                <button onClick={countHandler}>Count</button>
                 <button onClick={clearTextHandler}>Clear Text</button>
+            </div>
 
+            <div className="summary">
+                <h3>Your Text Summary</h3>
+                <p>You have typed {text.toString().split(" ").length} Words and {text.length} Characters.</p>
             </div>
 
         </div >
