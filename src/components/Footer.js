f9acd922cf201332ed06f0744af7db1cc183d7b1
Moksh Upadhyay <mokshupadhyay5@gmail.com>
2023-07-24 21:42:34 +0530

    recovered index.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,30 @@
-import React from "react";
-import "../css/Footer.css";
+import React from 'react';
+import '../css/Footer.css';
 
 const Footer = () => {
-	return (
-		<div className="footer">
-			<div className="about">
-				<h2 className="name">TextWizards</h2>
-				<hr className="line" />
-				<div className="content">
-					TextWizards is a simple Utility which help you change the appearance
-					of the text as you want.
-				</div>
-			</div>
-			<div className="links">
-				<ul className="nav-links">
-					<li>Google</li>
-					<li>Youtube</li>
-					<li>Instagram</li>
-					<li>Gmail</li>
-				</ul>
-			</div>
-		</div>
-	);
+    return (
+        <div className="footer">
+            <div className="about">
+                <h2 className="name">
+                    TextUtils
+                </h2>
+                <hr className="line" />
+                <div className="content">
+                    TextUtils is a simple Utility which help you change the appearance of the text as you want.
+                </div>
+            </div>
+            <div className="links">
+
+
+                <ul className="nav-links">
+                    <li>Google</li>
+                    <li>Youtube</li>
+                    <li>Instagram</li>
+                    <li>Gmail</li>
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default Footer;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Navbar from './components/Navbar';
+import TextUtil from './components/TextUtil';
+import Footer from './components/Footer';
+import './css/index.css';
+
+ReactDOM.render(
+	<React.StrictMode>
+		<Navbar title="TextUtils" />
+		<TextUtil />
+		<Footer />
+	</React.StrictMode>
+	,
+	document.getElementById('root')
+);
+
