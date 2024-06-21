d0d60401cd6ce62fb17d68b87554512a8bd02f5b
Moksh Upadhyay <mokshupadhyay5@gmail.com>
2024-06-21 12:31:16 +0530

    changed render method functionality, settings and package.json

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import Navbar from './components/Navbar';
 import TextUtil from "./components/TextWizard";
 import Footer from './components/Footer';
 import './css/index.css';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
 	<React.StrictMode>
 		<Navbar title="TextWizard" />
 		<TextUtil />
 		<Footer />
-	</React.StrictMode>,
-	document.getElementById("root")
+	</React.StrictMode>
 );
 
